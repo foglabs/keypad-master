e2959b16dd089bbff9265ab11e9652539a917f26
foglabs <foglaboratories@gmail.com>
2019-12-05 08:14:14 -0500

    'try'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// may not need this
+// import axios_lib from 'axios';
+// const axios = axios_lib.create({
+//   baseURL: BACKEND + ':4000',
+// });
+import React, { Component } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-function App() {
+// 'ws://localhost:8000'
+const BACKEND = 'ws://' + window.location.hostname + ':8000';
+const client = new W3CWebSocket(BACKEND);
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    // this.state = {
+    //   userid: null,
+    // }
+
+    // this is not necessary
+    this.playSound=this.playSound.bind(this);
+    console.log('cool')
+  }
+
+  componentWillMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connectedzz Master');
+    };
+
+    client.onmessage = (message) => {
+      console.log('da master message');
+      console.log(message.data);
+      let data = JSON.parse(message.data)
+      this.playSound(data.note)
+    };
+  }
+
+  playSound(note){
+    console.log('I fucking clicked stupid')
+    if(client.readyState === client.OPEN){
+      
+      
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        { sounds.map((sound) => <Sound sound={ sound } number={ sound }  /> ) }
+      </div>
+    );
+  }
+}
+
+function Sound (props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="sound" onClick={ props.onClick } >
+      { props.number + 1 }
     </div>
   );
 }
